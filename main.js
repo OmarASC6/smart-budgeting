aac36c7181253bc0ad93be539caebb346842462f
OmarASC6 <omartarekfawzy@gmail.com>
2019-08-12 16:16:47 -0400

    Co-authored-by: Darshan S <darshnyc@gmail.com> Co-authored-by: AlessanderTASC6 <Attu72289@gmail.com>m

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-subButton.addEventListener('click', getUserInput,false); 
+Button1.addEventListener('click', optionOne, false); 
+Button2.addEventListener('click', optionTwo, false);
+Button3.addEventListener('click', optionThree, false);
 
-function getUserInput(){
+function optionOne(){
     const userIncome = document.getElementById('nameField').value;
     const userExpenses = document.getElementById('fixedExpenses').value;
-    const remainder = userIncome - userExpenses;
-    let divide = 11.11;
-    let userSpend = Math.floor(remainder / divide);
-    if(remainder <= 10){
-    let divide = 1.11
-    let userSpend = Math.floor(remainder / divide)
-    }
-    console.log(userSpend);
+    const remainderOp1 = userIncome - userExpenses;
+    const multiplier1 = 0.14;
+    const spendMoneyOption1 = Math.floor(remainderOp1 * multiplier1);
+    console.log(spendMoneyOption1);
+}
+
+function optionTwo(){
+    const userIncome = document.getElementById('nameField').value;
+    const userExpenses = document.getElementById('fixedExpenses').value;
+    const remainderOp2 = userIncome - userExpenses;
+    const multiplier2 = 0.14;
+    const food = 0.25;
+    const spendMoneyOption2 = Math.floor((remainderOp2 - (remainderOp2 * food)) * multiplier2);
+    console.log(spendMoneyOption2);
+}
+
+function optionThree(){
+    const userIncome = document.getElementById('nameField').value;
+    const userExpenses = document.getElementById('fixedExpenses').value;
+    const remainderOp3 = userIncome - userExpenses;
+    const multiplier3 = 0.14;
+    const 
+    const spendMoneyOption3 = Math.floor(remainderOp3 * multiplier3);
+    console.log(spendMoneyOption3);
 }
