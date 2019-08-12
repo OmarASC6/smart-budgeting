7c17bc5fde3fc66b13fd333b4b166fa29d07bcec
AlessanderTASC6 <attu72289@gmail.com>
2019-08-12 12:15:30 -0400

    Co-authored-by: Darshan S <darshnyc@gmail.com> Co-authored-by: OmarASC6 <omartarekfawzy@gmail.com>

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
 subButton.addEventListener('click', getUserInput,false); 
 
-// function getUserIncome() {
-//     const userIncome = document.getElementById('nameField').value;
-//     return userIncome;
-// }
 function getUserInput(){
     const userIncome = document.getElementById('nameField').value;
     const userExpenses = document.getElementById('fixed-expenses').value;
     const remainder = userIncome - userExpenses;
-    
     const divide = 11.11;
     const userSpend = Math.floor(remainder / divide);
-
-
+    console.log(userSpend);
 }
-
