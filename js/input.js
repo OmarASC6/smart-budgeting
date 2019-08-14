da0707c4d8d12b99f852c3c3ae9be417b9144442
OmarASC6 <omartarekfawzy@gmail.com>
2019-08-14 16:04:38 -0400

    Co-authored-by: Darshan S <darshnyc@gmail.com> Co-authored-by: AlessanderTASC6 <Attu72289@gmail.com>

--- js/input.js
+++ js/input.js
@@ -18,8 +18,41 @@ function optionOne(){
     const utilities1 = Math.floor(remainderOp1 * 0.10);
     const emergency1 = Math.floor(remainderOp1 * 0.15);
     const education1 = Math.floor(remainderOp1 * 0.10);
-    document.getElementById("result").innerText = ("Food: " + food1 + "; Personal expenses: " + personalexpenses1 + "; Transportation: " + transportion1 + "; Medical: " + medical_expenses1 + "; Utilities:" + utilities1 + "; Emergency: " + emergency1 + "; Education: " + education1);
-}
+    document.getElementById("resultFood").innerText = (food1);
+    document.getElementById("resultPersonal").innerText = (personalexpenses1);
+    document.getElementById("resultTransport").innerText = (transportion1);
+    document.getElementById("resultMedical").innerText = (medical_expenses1);
+    document.getElementById("resultUtilities").innerText = (utilities1);
+    document.getElementById("resultEmergency").innerText = (emergency1);
+    document.getElementById("resultEducation").innerText = (education1);
+} 
+//     anychart.onDocumentReady(function() {
+//       // set the data
+//       const data = [
+//             {x: "Food", value: food1},
+//             {x: "personalexpenses", value: personalexpenses1},
+//             {x: "transportation", value: transportion1},
+//             {x: "medical", value: medical_expenses1},
+//             {x: "utilities", value: utilities1},
+//             {x: "emergency", value: emergency1},
+//             {x: "education", value: education1}
+//         ];
+      
+//         // create the chart
+//         const chart = anychart.pie();
+      
+//         // set the chart title
+//         chart.title("Population by Race for the United States: 2010 Census");
+      
+//         // add the data
+//         chart.data(data);
+      
+//         // display the chart in the container
+//         chart.container('container');
+//         chart.draw();
+// }
+
+
 
 function optionTwo(){
 //Food
@@ -34,7 +67,13 @@ function optionTwo(){
     const utilities2 = Math.floor(remainderOp2 * 0.10);
     const emergency2 = Math.floor(remainderOp2 * 0.15);
     const education2 = Math.floor(remainderOp2 * 0.10);
-    document.getElementById("result").innerText = ("Food: " + food2 + "; Personal expenses: " + personalexpenses2 + "; Transportation: " + transportion2 + "; Medical: " + medical_expenses2 + "; Utilities:" + utilities2 + "; Emergency: " + emergency2 + "; Education: " + education2);
+    document.getElementById("resultFood").innerText = (food2);
+    document.getElementById("resultPersonal").innerText = (personalexpenses2);
+    document.getElementById("resultTransport").innerText = (transportion2);
+    document.getElementById("resultMedical").innerText = (medical_expenses2);
+    document.getElementById("resultUtilities").innerText = (utilities2);
+    document.getElementById("resultEmergency").innerText = (emergency2);
+    document.getElementById("resultEducation").innerText = (education2);
 } 
 
 function optionThree(){
@@ -50,8 +89,13 @@ function optionThree(){
     const utilities3 = Math.floor(remainderOp3 * 0.10);
     const emergency3 = Math.floor(remainderOp3 * 0.15);
     const education3 = Math.floor(remainderOp3 * 0.10);
-    document.getElementById("result").innerHTML = ("Food: " + food3 + "; Personal expenses: " + personalexpenses3 + "; Transportation: " + transportion3 + "; Medical: " + medical_expenses3 + "; Utilities:" + utilities3 + "; Emergency: " + emergency3 + "; Education: " + education3);
-}
+    document.getElementById("resultFood").innerText = (food3);
+    document.getElementById("resultPersonal").innerText = (personalexpenses3);
+    document.getElementById("resultTransport").innerText = (transportion3);
+    document.getElementById("resultMedical").innerText = (medical_expenses3);
+    document.getElementById("resultUtilities").innerText = (utilities3);
+    document.getElementById("resultEmergency").innerText = (emergency3);
+    document.getElementById("resultEducation").innerText = (education3);}
 
 function optionFour(){
     //Medical
@@ -66,7 +110,13 @@ function optionFour(){
     const utilities4 = Math.floor(remainderOp4 * 0.10);
     const emergency4 = Math.floor(remainderOp4 * 0.15);
     const education4 = Math.floor(remainderOp4 * 0.10);
-    document.getElementById("result").innerHTML = ("Food: " + food4 + "; Personal expenses: " + personalexpenses4 + "; Transportation: " + transportion4 + "; Medical: " + medical_expenses4 + "; Utilities:" + utilities4 + "; Emergency: " + emergency4 + "; Education: " + education4);
+    document.getElementById("resultFood").innerText = (food4);
+    document.getElementById("resultPersonal").innerText = (personalexpenses4);
+    document.getElementById("resultTransport").innerText = (transportion4);
+    document.getElementById("resultMedical").innerText = (medical_expenses4);
+    document.getElementById("resultUtilities").innerText = (utilities4);
+    document.getElementById("resultEmergency").innerText = (emergency4);
+    document.getElementById("resultEducation").innerText = (education4);
 }
 
 function optionFive(){
@@ -82,7 +132,14 @@ function optionFive(){
     const utilities5 = Math.floor(remainderOp5 * 0.10);
     const emergency5 = Math.floor(remainderOp5 * 0.15);
     const education5 = Math.floor(remainderOp5 * 0.10);
-    document.getElementById("result").innerHTML = ("Food: " + food5 + "; Personal expenses: " + personalexpenses5 + "; Transportation: " + transportion5 + "; Medical: " + medical_expenses5 + "; Utilities:" + utilities5 + "; Emergency: " + emergency5 + "; Education: " + education5);
+    document.getElementById("resultFood").innerText = (food5);
+    document.getElementById("resultPersonal").innerText = (personalexpenses5);
+    document.getElementById("resultTransport").innerText = (transportion5);
+    document.getElementById("resultMedical").innerText = (medical_expenses5);
+    document.getElementById("resultUtilities").innerText = (utilities5);
+    document.getElementById("resultEmergency").innerText = (emergency5);
+    document.getElementById("resultEducation").innerText = (education5);
+    
 }
 
 function optionSix(){
@@ -98,5 +155,11 @@ function optionSix(){
     const utilities6 = Math.floor(remainderOp6 * 0.10);
     const emergency6 = Math.floor(remainderOp6 * 0.15);
     const education6 = Math.floor(remainderOp6 * 0.20);
-    document.getElementById("resultFood").innerText = ("Food: " + food6 + "; Personal expenses: " + personalexpenses6 + "; Transportation: " + transportion6 + "; Medical: " + medical_expenses6 + "; Utilities:" + utilities6 + "; Emergency: " + emergency6 + "; Education: " + education6);
+    document.getElementById("resultFood").innerText = (food6);
+    document.getElementById("resultPersonal").innerText = (personalexpenses6);
+    document.getElementById("resultTransport").innerText = (transportion6);
+    document.getElementById("resultMedical").innerText = (medical_expenses6);
+    document.getElementById("resultUtilities").innerText = (utilities6);
+    document.getElementById("resultEmergency").innerText = (emergency6);
+    document.getElementById("resultEducation").innerText = (education6);
 }
